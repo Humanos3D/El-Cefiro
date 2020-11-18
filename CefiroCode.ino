f13972437e2e031ee164c6e6f9311b44658ddccf
Mauricio Cano Bedoya <mauriciocanob@gmail.com>
2020-11-18 16:47:15 -0500

    Add files via upload

---
+++ CefiroCode.ino
@@ -0,0 +1,63 @@
+/*
+ Control Céfiro V1
+ For: Humanos3D-Enable Medellin
+ By: Mauricio Cano Bedoya June 2020 
+
+ Este código es para el control básico del servomotor con
+ un botón táctil para el dispositivo "Céfiro"
+
+*/
+// Declaraciones
+#include <Servo.h>
+#include <avr/sleep.h>
+#define PinBoton 11 
+#define PinServo 10
+Servo servo; 
+
+// Variables
+int Estado = 0; 
+const int POS1 = 165;
+const int POS2 = 20;
+const int TIME_OUT1 = 100;
+const int TIME_SLEEP = 30000;
+int FLAG = 0;     
+unsigned long time;   
+
+// Configuración 
+void setup() 
+{
+  //Serial.begin(9600);
+  servo.attach(PinServo);
+  pinMode(PinBoton, INPUT); 
+}
+
+void loop() 
+{
+  // Modo de bajo consumo luego de 30 segundos
+  time = millis();
+  if (time > TIME_SLEEP) sleep_mode();
+  
+  // Lectura estado del botón
+  Estado = digitalRead(PinBoton);
+  
+  // Si el botón es presionado el motor se activa
+  if (Estado == HIGH)
+  {    
+    servo.attach(PinServo);
+    servo.write(POS1); 
+    delay(TIME_OUT1);
+    servo.detach();
+    FLAG = 1;
+  } else 
+  // De lo contrario vuelve a su posición original  
+  {
+    if (FLAG)
+    {
+      FLAG = 0;
+      servo.attach(PinServo);
+      servo.write(POS2);
+      delay(TIME_OUT1);
+      servo.detach();
+    }
+  } 
+}
---
+++ CefiroCodeV2.ino
@@ -0,0 +1,67 @@
+// For: Humanos3D-Enable Medellin
+// By: Mauricio Cano June 2020
+
+// Control Céfiro V2
+// Este código es para el control básico del servomotor con
+// un botón táctil para el dispositivo "Céfiro"
+
+// Declaraciones
+#include <Servo.h>
+#include <avr/sleep.h>
+#define PinBoton 11 
+#define PinServo 10
+Servo servo; 
+
+// Variables
+int Estado = 0; 
+const int POS1 = 165;
+const int POS2 = 20;
+const int TIME_OUT1 = 100;
+const int TIME_OUT2 = 1000;
+const int TIME_SLEEP = 30000;
+int band = 1;
+bool FLAG = false;   
+unsigned long time;  
+
+// Configuración
+void setup() {
+  //Serial.begin(9600);
+  servo.attach(PinServo);
+  pinMode(PinBoton, INPUT); 
+}
+
+void loop() {
+  
+  // Modo de bajo consumo luego de 30 segundos
+  time = millis();
+  if (time > TIME_SLEEP) sleep_mode();
+  
+  // Lectura estado del botón
+  Estado = digitalRead(PinBoton);
+  if (Estado == HIGH)
+  {
+    FLAG = !FLAG;
+    band = 1;
+    delay(TIME_OUT2);
+  }
+
+  // Condiciones que activan el servomotor en las diferentes posiciones
+  if (FLAG && band)
+  {
+    //Serial.println("Posicion 1");
+    band = 0;
+    servo.attach(PinServo);
+    servo.write(POS1);
+    delay(TIME_OUT1);
+    servo.detach();
+  }
+  else if (FLAG == 0 && band)
+  {
+    //Serial.println("Posicion 2");
+    band = 0;
+    servo.attach(PinServo);
+    servo.write(POS2);
+    delay(TIME_OUT1);
+    servo.detach();
+  }
+}
